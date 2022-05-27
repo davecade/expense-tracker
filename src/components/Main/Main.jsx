62bb748dcdd657a8cde745ba3b357f4e92ac7f84
Dave <dave.cadelina@outlook.com>
2022-05-28 01:45:21 +1000

    added main containers

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Main.styles.scss'
+
+const Main = () => {
+  return (
+    <div className="main-container">
+        <div className="heading-container">
+
+        </div>
+        <div className="total-amount-container">
+
+        </div>
+        <div className="list-container">
+          
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.styles.scss'
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+
+    </div>
+  )
+}
+
+export default Navbar
