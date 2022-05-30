ddb6aa5d52d2ed4162c7a262412672373726d772
Dave <dave.cadelina@outlook.com>
2022-05-30 20:52:02 +1000

    added bit of color and placeholders

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,11 +4,16 @@ import './Main.styles.scss'
 const Main = () => {
   return (
     <div className="main-container">
+
         <div className="heading-container">
-
+          <h3>Category Name</h3>
+          <h4 className="user-account">
+            Dave Cadelina
+          </h4>
         </div>
-        <div className="total-amount-container">
 
+        <div className="total-amount-container">
+            
         </div>
         <div className="list-container">
           
